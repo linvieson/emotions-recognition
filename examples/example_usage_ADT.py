466c321097b1d51c4b1fb412026110a7338b2bf4
Fox <you@example.com>
2021-04-29 01:45:53 +0300

    add example_usage_ADT.py && create diagrame of classes

---
+++ examples/example_usage_ADT.py
@@ -0,0 +1,77 @@
+from pprint import pprint
+
+import requests
+from PIL import Image as Picture
+
+from ..modules.classes import InstagramPage, Image, Emotion
+
+try:
+    ###########################################################################
+    #                     example of usage Emotion ADT                        #
+    ###########################################################################
+    print('test Emotion ADT')
+
+    emotion_ex = Emotion('angry', 1.)
+
+    print(emotion_ex)
+    '''
+    emotion name:                    angry
+    emotion percentage:              100.0%
+    life average emotion percentage: 99.9%'
+    '''
+
+    ###########################################################################
+    #                      example of usage Image ADT                         #
+    ###########################################################################
+    print('\n' + '-' * 89 + '\ntest Image ADT')
+
+    url_photo = 'https://img.apmcdn.org/d947173a53458e5298472d9450325e7adc13468f/widescreen/60636c-20190625-angry.jpg'
+    picture = Image(url_photo)
+
+    print('show pictures')
+    picture.picture.show()
+
+    print('num of emotions:', len(picture.emotions))  # 8
+    # next loop print info about 8 types of emotion in this photo:
+    # anger, contempt, disgust, fear, happiness, neutral, sadness, surprise
+    print('info about emotion')
+    for emotion in picture.emotions:
+        print(emotion)
+        print('---------------------------------------')
+
+    print(f'info about rectangle\n'
+          f'The distance to the left side of the image from rectangle is: {picture.rectangle.left}\n'
+          f'The distance to the top of the image from rectangle is: {picture.rectangle.top}\n'
+          f'The height of rectangle is: {picture.rectangle.height}\n'
+          f'The width of rectangle is: {picture.rectangle.width}')
+
+    ###########################################################################
+    #                  example of usage InstagramPage ADT                     #
+    ###########################################################################
+    print('\n' + '-' * 89 + '\ntest InstagramPage ADT')
+
+    page = InstagramPage('f.o.x.109')
+
+    print('show pictures')
+    page.photos[0].picture.show()  # show first photo from this profile
+    page.happiest_photo.show()  # show happiest photo from this profile
+
+    print('info about emotion')
+    # next loop print info about average emotions in Instagram page
+    # anger, contempt, disgust, fear, happiness, neutral, sadness, surprise
+    for emotion in page.average_emotions:
+        print(emotion)
+        print('---------------------------------------')
+
+    print(f'relative fakeness percentage: {round(100 * page.relative_fakeness(), 1)}')
+    print('print info about emotions of Instagram page')
+    pprint(page.analyze)
+
+    print('print report into the file')
+    page.write_to_file()
+
+    print('visualise report')
+    page.visualize()
+
+except Exception as err:
+    print(err)
--- modules/classes.py
+++ modules/classes.py
@@ -1,47 +1,126 @@
 """ module with implementation ADT, which are used in project """
+from typing import List
+
+import requests
+from PIL import Image as Picture
+
+
+class Emotion:
+    """ class Emotion """
+    def __init__(self, emotion: str, percentage: float):
+        self.__emotions = emotion
+        self.__percentage = percentage
+        self.__life_average = None  # type - float
+        self.__get_life_average()
+
+    def __get_life_average(self):
+        """ parse life average emotion percentage by name """
+        if self.__emotions == '':
+            self.__life_average = 1.
+        else:
+            raise ValueError
+
+    @property
+    def emotions(self):
+        """ get privat attribute __emotions """
+        return self.__emotions
+
+    @property
+    def percentage(self):
+        """ get privat attribute __percentage """
+        return self.__percentage
+
+    @property
+    def life_average(self):
+        """ get privat attribute __life_average """
+        return self.__life_average
+
+    def __str__(self):
+        return \
+          f'emotion name:                    {self.emotions}\n'\
+          f'emotion percentage:              {round(100*self.percentage,1)}\n'\
+          f'life average emotion percentage: {round(100*self.life_average,1)}'
+
+
+class Image:
+    """ class InstagramPage """
+    def __init__(self, link: str):
+        self.__link = link
+        self.__all_attributes = {}  # type - dict
+        self.__emotions = []        # type - List[Emotion]
+        self.__picture = None       # type - Picture
+        self.__rectangle = None     # type  - Coordinate
+        self.__parse_image_info()
+
+    def __parse_image_info(self):
+        """ parse info of Instagram page """
+        self.__all_attributes = {}
+        self.__emotions = []
+        self.__picture = Picture.open(requests.get(self.__link, stream=True).raw)
+        self.__rectangle = None
+
+    @property
+    def all_attributes(self):
+        """ get privat attribute __all_attributes """
+        return self.__all_attributes
+
+    @property
+    def emotions(self):
+        """ get privat attribute __emotions """
+        return self.__emotions
+
+    @property
+    def picture(self):
+        """ get privat attribute __picture """
+        return self.__picture
+
+    @property
+    def rectangle(self):
+        """ get privat attribute __rectangle """
+        return self.__rectangle
 
 
 class InstagramPage:
     """ class InstagramPage """
-    def __init__(self, link):
+    def __init__(self, link: str):
         self.__link = link
-        self.__photos = []
-        self.__happiest_photo = None
-        self.__average_emotions = None
+        self.__photos = []              # type - List[Image]
+        self.__happiest_photo = None    # type - Picture
+        self.__average_emotions = []    # type - List[Emotion]
         self.__parse_page_info()
 
     def __parse_page_info(self):
         """ parse info of Instagram page """
         self.__photos = []
         self.__happiest_photo = None
-        self.__average_emotions = None
+        self.__average_emotions = []
 
     @property
     def photos(self):
-        """ get privet attribute __photos """
+        """ get privat attribute __photos """
         return self.__photos
 
     @property
     def happiest_photo(self):
-        """ get privet attribute __happiest_photo """
+        """ get privat attribute __happiest_photo """
         return self.__happiest_photo
 
     @property
     def average_emotions(self):
-        """ get privet attribute __average_emotions """
+        """ get privat attribute __average_emotions """
         return self.__average_emotions
 
-    def relative_fakeness(self):
+    def relative_fakeness(self) -> float:
         """
-        will return the relative percentage of fake profile emotion
+        return the relative percentage of fake profile emotion
         compared to the average human emotions
         """
-        pass
+        return 1.
 
-    def analyze(self):
+    def analyze(self) -> List[Emotion]:
         """
-        will analyze the profile photo and return the dictionary, where the
-        key will be the emotion, and the value of its percentage in the profile
+        will analyze the profile photo and return a list with the Emotion
+        objects, which describe the profile
         """
         pass
 
@@ -56,72 +135,3 @@ class InstagramPage:
         display profile statistics in graphs and diagrams
         """
         pass
-
-
-class Image:
-    """ class InstagramPage """
-    def __init__(self, link):
-        self.__link = link
-        self.__all_attributes = {}
-        self.__emotions = []
-        self.__picture = None
-        self.__rectangle = None
-        self.__parse_link()
-
-    def __parse_image_info(self):
-        """ parse info of Instagram page """
-        self.__all_attributes = {}
-        self.__emotions = []
-        self.__picture = None
-        self.__rectangle = None
-
-    @property
-    def all_attributes(self):
-        """ get privet attribute __all_attributes """
-        return self.__all_attributes
-
-    @property
-    def emotions(self):
-        """ get privet attribute __emotions """
-        return self.__emotions
-
-    @property
-    def picture(self):
-        """ get privet attribute __picture """
-        return self.__picture
-
-    @property
-    def rectangle(self):
-        """ get privet attribute __rectangle """
-        return self.__rectangle
-
-
-class Emotion:
-    """ class Emotion """
-    def __init__(self, emotion, percentage):
-        self.__emotions = emotion
-        self.__percentage = percentage
-        self.__life_average = None
-        self.__get_life_average()
-
-    def __get_life_average(self):
-        """ parse life average emotion percentage by name """
-        if self.__emotions == '':
-            self.__life_average = 1.
-        else:
-            raise ValueError
-
-    @property
-    def emotions(self):
-        """ get privet attribute __emotions """
-        return self.__emotions
-
-    @property
-    def percentage(self):
-        """ get privet attribute __percentage """
-        return self.__percentage
-
-    @property
-    def life_average(self):
-        """ get privet attribute __life_average """
-        return self.__life_average
