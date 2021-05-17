ba21c1bddba629509e1ac40bb9c2620d1188722b
Fox <you@example.com>
2021-05-17 16:39:17 +0300

    fix button and it finally work

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import os
 from pathlib import Path
-from flask import Flask, render_template, request, url_for, redirect, send_from_directory,send_file
+from flask import Flask, render_template, request, url_for, send_file
 
 from modules.classes import InstagramPage
 
 app = Flask(__name__)
-
 
 
 @app.context_processor
@@ -42,35 +41,47 @@ def emotions():
     """
     Show the twitter map in the internet
     """
-    # try:
-    nickname = request.form.get("nickname")
-    if nickname:
-        try:
-            Path(os.path.join(os.getcwd(), 'static', 'data', f'{nickname}')).mkdir()
-            inst = InstagramPage(nickname)
-            os.chdir(os.path.join(os.getcwd(), 'static', 'data', f'{nickname}'))
-            inst.visualize()
-            inst.happiest_photo.save(f'{nickname}_happiest_photo.png', 'png')
-            fakeness = inst.relative_fakeness()
-            emoji_path = inst.get_emoji()
-            with open('emoji_and_fakeness.txt', 'w') as file:
-                file.write(str(fakeness))
-                file.write('\n')
-                file.write(emoji_path)
-            os.chdir(os.path.join('..', '..', '..'))
-        except FileExistsError:
-            with open(os.path.join(os.getcwd(), 'static', 'data', f'{nickname}', 'emoji_and_fakeness.txt'), 'r') as file:
-                fakeness = file.readline()
-                emoji_path = file.readline()
+    try:
+        nickname = request.form.get("nickname")
+        if nickname:
+            try:
+                Path(os.path.join(os.getcwd(), 'static', 'data', f'{nickname}')).mkdir()
+                inst = InstagramPage(nickname)
+                os.chdir(os.path.join(os.getcwd(), 'static', 'data', f'{nickname}'))
+                inst.visualize()
+                inst.zip_result()
+                inst.happiest_photo.save(f'{nickname}_happiest_photo.png', 'png')
+                fakeness = inst.relative_fakeness()
+                emoji_path = inst.get_emoji()
+                with open('emoji_and_fakeness.txt', 'w') as file:
+                    file.write(str(fakeness))
+                    file.write('\n')
+                    file.write(emoji_path)
+                os.chdir(os.path.join('..', '..', '..'))
+            except FileExistsError:
+                with open(os.path.join(os.getcwd(), 'static', 'data', f'{nickname}', 'emoji_and_fakeness.txt'), 'r') as file:
+                    fakeness = file.readline()
+                    emoji_path = file.readline()
 
-        happiest_path = os.path.join('..', 'static', 'data', f'{nickname}', f'{nickname}_happiest_photo.png')
-        emotion_graphic_path = os.path.join('..', 'static', 'data', f'{nickname}', f'{nickname}_emotion_graphic.png')
-        emotion_piechart_path = os.path.join('..', 'static', 'data', f'{nickname}', f'{nickname}_emotion_piechart.png')
+            happiest_path = os.path.join('..', 'static', 'data', f'{nickname}',
+                                         f'{nickname}_happiest_photo.png')
+            emotion_graphic_path = os.path.join('..', 'static', 'data',
+                                                f'{nickname}',
+                                                f'{nickname}_emotion_graphic.png')
+            emotion_piechart_path = os.path.join('..', 'static', 'data',
+                                                 f'{nickname}',
+                                                 f'{nickname}_emotion_piechart.png')
 
-        return render_template("emotions.html", data={'nickname': nickname, 'fakeness': fakeness, 'happiest': happiest_path, 'emoji': emoji_path,
-                                                        'diagram_1': emotion_graphic_path, 'diagram_2': emotion_piechart_path})
-    # except Exception:
-    #     return render_template('failure.html')
+            return render_template("emotions.html",
+                                   data={'nickname': nickname,
+                                         'fakeness': fakeness,
+                                         'happiest': happiest_path,
+                                         'emoji': emoji_path,
+                                         'diagram_1': emotion_graphic_path,
+                                         'diagram_2': emotion_piechart_path})
+    except Exception:
+        return render_template('failure.html')
+
 
 @app.route('/uploads/<path:filename>', methods=['GET', 'POST'])
 def download(filename):
@@ -79,7 +90,8 @@ def download(filename):
     # uploads = os.path.join(app.root_path, app.config[UPLOAD_DIRECTORY])
     # Returning file from appended path
     print(filename)
-    return send_file( filename, as_attachment=True)
+    return send_file(filename, as_attachment=True)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
